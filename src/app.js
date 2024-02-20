ee37be1f5948363918ff5f9199a5439dc2c4b3c6
arnoldkaroki <145427394+arnoldkaroki@users.noreply.github.com>
2024-02-20 06:17:58 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,48 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import TransactionTable from './TransactionTable';
 import TransactionForm from './TransactionForm';
-import SearchBar from './SearchBar';
+import SearchBar from "./SearchBar"
+import './App.css'
 
-function App() {
+const App = () => {
   const [transactions, setTransactions] = useState([]);
-  const [filteredTransactions, setFilteredTransactions] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:8001/transactions')
-      .then(response => response.json())
-      .then(data => {
-        setTransactions(data.transactions);
-        setFilteredTransactions(data.transactions);
-      })
-      .catch(error => console.error('Error fetching data:', error));
+    fetchTransactions();
   }, []);
 
-  const handleAddTransaction = (newTransaction) => {
-    setTransactions(prevTransactions => [...prevTransactions, newTransaction]);
-    setFilteredTransactions(prevTransactions => [...prevTransactions, newTransaction]);
+  const fetchTransactions = async () => {
+    try {
+      const response = await fetch('https://ph2-wk1-server.onrender.com/transactions');
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const transactionsArray = data.transactions || data;
+
+      setTransactions(transactionsArray);
+    } catch (error) {
+      console.error('Error fetching transactions:', error);
+    }
   };
 
-  const handleSearchChange = (term) => {
-    setSearchTerm(term);
-    const filtered = transactions.filter(transaction =>
-      transaction.description.toLowerCase().includes(term.toLowerCase())
-    );
-    setFilteredTransactions(filtered);
+  const handleAddTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
   };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>bankofflatiron</h1>
-      </header>
-      <main>
-        <TransactionForm onAddTransaction={handleAddTransaction} />
-        <SearchBar searchTerm={searchTerm} onSearchChange={handleSearchChange} />
-        <TransactionTable transactions={filteredTransactions} />
-      </main>
+    <div id='parentdiv'>
+      <div id='Siteheader'><h1>Flat ironBank</h1></div>
+      <TransactionForm onAddTransaction={handleAddTransaction} />
+      <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <TransactionTable transactions={filteredTransactions} />
     </div>
   );
-}
+};
 
 export default App;
